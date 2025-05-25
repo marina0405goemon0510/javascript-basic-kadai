074ea2e578dec279b27057fd0f65df300af72f4c
marina0405goemon0510 <marinaspace194@gmail.com>
2025-05-25 21:26:31 +0900

    kadai_mdn_js

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const date = new Date("2025-05-25T21:13:30");
+const [month, day, year] = [
+  date.getMonth(6),
+  date.getDate(25),
+  date.getFullYear(2025),
+];
+
+
+const formattedDate = 2025+ '年' + 5 + '月' + 25 + '日';
+console.log(formattedDate);
