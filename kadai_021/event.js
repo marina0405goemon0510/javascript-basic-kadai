520f034805c7371e66b789aa61fcc9dabc23282f
marina0405goemon0510 <marinaspace194@gmail.com>
2025-05-31 17:27:13 +0900

    kadai_021

---
+++ kadai_021/event.js
@@ -0,0 +1,13 @@
+// btn、textというidを持つHTML要素を取得し、定数に代入する
+const Btn = document.getElementById('btn');
+const text=document.getElementById('text');
+
+
+// HTML要素がクリックされたときにイベント処理を実行する
+  Btn.addEventListener('click', () => {
+     // クリックされたときに二秒後に文字を変更するidを取得する
+    setTimeout(() => {
+      text.textContent="ボタンをクリックしました"
+    }, 2000);
+  })
+
